54112f4f80d2809f0d8c5ec1b959b54daeb6cb35
Bravilogy <bravilogy@gmail.com>
2018-06-27 14:23:41 +0100

    show download progress and clear screen after each download

--- src/radio105/core.clj
+++ src/radio105/core.clj
@@ -7,6 +7,21 @@
 
 (def urls #{"http://radio105.wanex.net/arts_geo.htm" "http://radio105.wanex.net/songs_geo.htm"})
 
+(defn- clear-screen
+  []
+  (print (str (char 27) "[2J"))
+  (print (str (char 27) "[;H")))
+
+(defn- show-download-progress
+  [current total]
+  (clear-screen)
+  (let [complete (->> (/ current total)
+                      (* 100)
+                      Math/ceil
+                      int)]
+    (println
+     (str (if (> complete 80) "Almost there " "Completed ") complete "%"))))
+
 (defn- get-dom
   [url]
   (html/html-snippet (:body @(http/get url))))
@@ -14,35 +29,45 @@
 (defn- build-url
   [audio-link]
   {:download-url (format "http://radio105.wanex.net/%s" audio-link)
-   :filename audio-link})
-
-(defn- extract-links
-  [dom]
-  (map #(get-in % [:attrs :href]) (html/select dom [:td :a])))
-
-(defn- download-file
-  [{:keys [download-url filename] :as file}]
-  (let [filename (string/replace filename #"\/" "_")
-        file (io/file "audio" filename)]
-    (if-not (.exists file)
-      (do
-        (println "Downloading" filename)
-        (with-open [in (io/input-stream download-url)
-                    out (io/output-stream file)]
-          (io/copy in out)))
-      (println "File" filename "already exists, skipping"))))
+   :filename     audio-link})
 
 (defn- is-mp3?
   [link]
   (re-matches #"(?i).*\.mp3" link))
+
+(defn- extract-links
+  [dom]
+  (->> (html/select dom [:td :a])
+       (map #(get-in % [:attrs :href]))
+       (filter is-mp3?)))
+
+(defn- download-file
+  [{:keys [download-url filename]}]
+  (let [filename (string/replace filename #"\/" "_")
+        file     (io/file "audio" filename)]
+    (if-not (.exists file)
+      (do
+        (println "Downloading" filename)
+        (with-open [in  (io/input-stream download-url)
+                    out (io/output-stream file)]
+          (io/copy in out)))
+      (println "File" filename "already exists, skipping"))))
+
+(defn- download-files
+  [files]
+  (let [total-count (count files)]
+    (doall
+     (for [[file index] (mapv vector files (range))
+           :let         [current (+ index 1)]]
+       (do
+         (show-download-progress current total-count)
+         (download-file file))))))
 
 (defn -main
   [& args]
   (when-not (.isDirectory (io/file "audio"))
     (.mkdir (io/file "audio")))
   (some->> urls
-           (mapcat get-dom)
-           extract-links
-           (filter is-mp3?)
+           (mapcat (comp extract-links get-dom))
            (map build-url)
-           (run! download-file)))
+           download-files))
